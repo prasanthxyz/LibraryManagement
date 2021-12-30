75a2e2ca31f41de5ef92353141f1e2f6bbac5da8
Prasanth P <prasanthxyz@gmail.com>
2021-12-30 16:49:22 +0530

    Remove DataType and split read/write functions in StorageManager

--- LibraryLib/src/main/java/com/pp/librarymanagement/lib/DataType.java
+++
@@ -1,7 +0,0 @@
-package com.pp.librarymanagement.lib;
-
-public enum DataType {
-    BOOK,
-    USER,
-    USEREVENT
-}
--- LibraryLib/src/main/java/com/pp/librarymanagement/lib/IStorageManager.java
+++ LibraryLib/src/main/java/com/pp/librarymanagement/lib/IStorageManager.java
@@ -1,6 +1,12 @@
 package com.pp.librarymanagement.lib;
 
+import java.util.List;
+
 public interface IStorageManager {
-    <T> T read(DataType dataType);
-    <T> void write(T data, DataType dataType);
+    List<Book> readBooks();
+    void writeBooks(List<Book> books);
+    List<User> readUsers();
+    void writeUsers(List<User> users);
+    List<UserEvent> readUserEvents();
+    void writeUserEvents(List<UserEvent> userEvents);
 }
